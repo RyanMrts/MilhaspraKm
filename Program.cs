ac9717358f356be936a44f8c20850cb9e844db91
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-02 14:08:09 -0300

    exercício pronto

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double milhas, kilometros;
+
+Console.Write("digte a medida desejada em milhas: ");
+milhas = Convert.ToDouble(Console.ReadLine());
+
+kilometros = milhas * 1.609;
+Console.WriteLine($"em Km:  {kilometros}" );
